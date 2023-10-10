9b7c84e52ed3bd0e21aa37b3a80ebdceff7f6c1d
Basith5075 <basith.5075@gmail.com>
2023-10-10 03:50:29 -0500

    Modified Rooms endpoint to accept json properties in the Amenities column, refactored the code, added comments

--- src/main/java/com/hotel/controller/RoomInfoController.java
+++ src/main/java/com/hotel/controller/RoomInfoController.java
@@ -1,5 +1,7 @@
 package com.hotel.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hotel.model.Room;
 import com.hotel.service.RoomService;
 import jakarta.validation.Valid;
@@ -8,7 +10,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
+
+/**
+ * Controller class for the room endpoint handling the create, update and get operations.
+ *
+ * @author Abdul Basith
+ */
+
 
 @RestController
 @Validated
@@ -22,19 +32,26 @@ public class RoomInfoController {
         this.roomService = roomService;
     }
 
-//	createRoom() method/endpoint handles a POST request to create a Room by accepting a Room object in the request body,
+    //	createRoom() method/endpoint handles a POST request to create a Room by accepting a Room object in the request body,
 //	calling the createRoom method of roomService, 
 //	and returning the created Room object in the response with an HTTP status code of 200 (OK).
     @RequestMapping(path = "/create", method = RequestMethod.POST)
-    public ResponseEntity<Room> createRoom(@Valid @RequestBody Room room) {
+    public ResponseEntity<Room> createRoom(@Valid @RequestBody String roomData) throws JsonProcessingException {
 
-            return new ResponseEntity<Room>(roomService.createRoom(room), HttpStatus.OK);
-        }
+        // Initialize an ObjectMapper to parse JSON data
+        ObjectMapper objectMapper = new ObjectMapper();
+        // Deserialize the JSON data into a Room object
+        Room room = objectMapper.readValue(roomData, Room.class);
+        // Call the service to create the room and get the saved room
+        Room savedRoom = roomService.createRoom(room);
+        // Return a ResponseEntity with the saved room and OK status
+        return new ResponseEntity<Room>(savedRoom, HttpStatus.OK);
+    }
 
     //	getRoomById() method/endpoint handles a GET request to get a Room by its id using Service layer
     @RequestMapping(path = "/get/{id}", method = RequestMethod.GET)
     public ResponseEntity<Room> getRoomById(@Valid @PathVariable int id) {
-            return new ResponseEntity<Room>(roomService.getRoombyId(id), HttpStatus.OK);
+        return new ResponseEntity<Room>(roomService.getRoombyId(id), HttpStatus.OK);
 
     }
 
@@ -42,7 +59,7 @@ public class RoomInfoController {
     @RequestMapping(path = "/get", method = RequestMethod.GET)
     public ResponseEntity<List<Room>> getAllRooms() {
         List<Room> roomList = roomService.getAllRoom();
-        return new ResponseEntity<>(roomList,HttpStatus.OK);
+        return new ResponseEntity<>(roomList, HttpStatus.OK);
     }
 
     // updateRoom() method handles the patch request and is used to update the room
@@ -50,8 +67,10 @@ public class RoomInfoController {
 
 
     @RequestMapping(path = "/update/{id}", method = RequestMethod.PATCH)
-    public ResponseEntity<Room> updateRoom(@PathVariable int id,@RequestBody Room room) {
+    public ResponseEntity<Room> updateRoom(@PathVariable int id, @RequestBody String roomData) throws JsonProcessingException {
 
-            return new ResponseEntity<Room>(roomService.updateRoomByFields(id, room), HttpStatus.OK);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Room room = objectMapper.readValue(roomData, Room.class);
+        return new ResponseEntity<Room>(roomService.updateRoomByFields(id, room), HttpStatus.OK);
     }
 }
